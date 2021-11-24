991af9c602526dd68cc396fa8f47ecb5afd95eb3
scristo2 <sergio_colino_c@hotmail.com>
2021-11-25 00:48:03 +0100

    update code 25/11/21

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ export class App{
 
     constructor(){
 
-         alert('this is the App');
+         this.addStyle();
+         
+    }
+
+
+    addStyle(){
+
+        var head = document.querySelectorAll('head')[0];
+        var style = document.createElement('link');
+        style.href = "./src/index.css?cache=345";
+        style.rel = "stylesheet";
+        style.type = "text/css";
+        head.appendChild(style);
     }
 }
